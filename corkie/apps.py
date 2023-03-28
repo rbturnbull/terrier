bfe2d143764fc3b4a605dd7ed4ef34ff1b6d6270
rturnbull <robert.turnbull@unimelb.edu.au>
2023-03-28 22:59:52 +1100

    :zap: adding Hierarchical softmax

--- corkie/apps.py
+++ corkie/apps.py
@@ -1,14 +1,43 @@
+from functools import partial
 from pathlib import Path
 from torch import nn
 from fastai.data.core import DataLoaders
 import torchapp as fa
 from rich.console import Console
 console = Console()
+from hierarchicalsoftmax import HierarchicalSoftmaxLoss, SoftmaxNode
+from hierarchicalsoftmax import metrics
+import csv
+
 
 class Corkie(fa.TorchApp):
     """
     Classifier of Repeats
     """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.classification_tree = SoftmaxNode(name="root")
+        self.classification_nodes = {}
+        with open(Path(__file__).parent/"data/TEClasses.tsv") as f:
+            reader = csv.DictReader(f, delimiter='\t')
+            for row in reader:
+                lineage_string = row['full_name']
+                lineage = lineage_string.split(";")
+                if len(lineage) == 1:
+                    parent = self.classification_tree
+                else:
+                    parent_lineage_string = ";".join(lineage[:-1])
+                    parent = self.classification_nodes[parent_lineage_string]
+                
+                node = SoftmaxNode(name=lineage[-1], parent=parent, title=row['title'])
+                self.classification_nodes[lineage_string] = node
+
+        try:
+            self.classification_tree.render(filepath="dfam.svg")
+            self.classification_tree.render(filepath="dfam.png")
+        except:
+            print("Cannot render classification tree")
+
     def dataloaders(
         self,
         inputs:Path = fa.Param(help="The input file."), 
@@ -26,15 +55,24 @@ class Corkie(fa.TorchApp):
         """
         raise NotImplemented("Dataloaders function not implemented yet.") 
 
-    def model(
-        self,
-    ) -> nn.Module:
-        """
-        Creates a deep learning model for the Corkie to use.
+    def loss_func(self):
+        return HierarchicalSoftmaxLoss(root=self.classification_tree)
 
-        Returns:
-            nn.Module: The created model.
-        """
-        raise NotImplemented("Model function not implemented yet.") 
-        return nn.Sequential(
-        )
+    def metrics(self):
+        return [
+            partial(metrics.greedy_accuracy, root=self.classification_tree), 
+            partial(metrics.greedy_f1_score, root=self.classification_tree),
+            partial(metrics.greedy_accuracy_depth_one, root=self.classification_tree), 
+            partial(metrics.greedy_accuracy_depth_two, root=self.classification_tree), 
+        ]
+
+    def monitor(self):
+        return "greedy_accuracy_depth_two"
+
+    def output_results(
+        self, 
+        results, 
+        **kwargs
+    ):
+        print(results)
+        return results
