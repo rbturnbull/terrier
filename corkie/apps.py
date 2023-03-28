b56b215ad29ffd058be90f27d8d8be9c51d9ba33
rturnbull <robert.turnbull@unimelb.edu.au>
2023-03-29 00:06:29 +1100

    :zap: adding components to get it to have a dataloader and model

--- corkie/apps.py
+++ corkie/apps.py
@@ -2,15 +2,49 @@ from functools import partial
 from pathlib import Path
 from torch import nn
 from fastai.data.core import DataLoaders
-import torchapp as fa
+import torchapp as ta
 from rich.console import Console
+from fastai.data.block import DataBlock, TransformBlock
+from fastai.data.transforms import RandomSplitter
 console = Console()
 from hierarchicalsoftmax import HierarchicalSoftmaxLoss, SoftmaxNode
 from hierarchicalsoftmax import metrics
 import csv
+from .famdb import FamDB
+from corgi.tensor import dna_seq_to_tensor
+from corgi.models import ConvClassifier
 
 
-class Corkie(fa.TorchApp):
+class DictionaryGetter:
+    def __init__(self, dictionary):
+        self.dictionary = dictionary
+
+    def __call__(self, key):
+        value = self.dictionary[key]
+        return value
+
+
+class SequenceGetter():
+    def __init__(self, famdb):
+        self.famdb = famdb
+
+    def __call__(self, accession:str):
+        family = self.famdb.get_family_by_accession(accession)
+        return dna_seq_to_tensor(family.consensus)
+
+
+class ClassificationGetter():
+    def __init__(self, famdb, classification_nodes):
+        self.famdb = famdb
+        self.classification_nodes = classification_nodes
+
+    def __call__(self, accession:str):
+        family = self.famdb.get_family_by_accession(accession)
+        return dna_seq_to_tensor(family.consensus)
+
+
+
+class Corkie(ta.TorchApp):
     """
     Classifier of Repeats
     """
@@ -42,8 +76,8 @@ class Corkie(fa.TorchApp):
 
     def dataloaders(
         self,
-        inputs:Path = fa.Param(help="The input file."), 
-        batch_size:int = fa.Param(default=32, help="The batch size."),
+        famdb:Path = ta.Param(help="The FamDB file (hdf5) input file from Dfam."), 
+        batch_size:int = ta.Param(default=1, help="The batch size."),
     ) -> DataLoaders:
         """
         Creates a FastAI DataLoaders object which Corkie uses in training and prediction.
@@ -55,7 +89,24 @@ class Corkie(fa.TorchApp):
         Returns:
             DataLoaders: The DataLoaders object.
         """
-        raise NotImplemented("Dataloaders function not implemented yet.") 
+        self.famdb = FamDB(famdb)
+        accessions = self.famdb.get_family_accessions()
+        self.accession_to_node_id = {}
+
+        for accession in accessions:
+            family = self.famdb.get_family_by_accession(accession)
+            node = self.classification_nodes[family.classification]
+            node_id = self.classification_tree.node_to_id[node]
+            self.accession_to_node_id[accession] = node_id
+
+        datablock = DataBlock(
+            blocks=[TransformBlock, TransformBlock],
+            get_x=SequenceGetter(famdb=self.famdb),
+            get_y=DictionaryGetter(self.accession_to_node_id),
+            splitter=RandomSplitter(),
+        )
+
+        return datablock.dataloaders(accessions, bs=batch_size)
 
     def loss_func(self):
         return HierarchicalSoftmaxLoss(root=self.classification_tree)
@@ -68,8 +119,88 @@ class Corkie(fa.TorchApp):
             partial(metrics.greedy_accuracy_depth_two, root=self.classification_tree), 
         ]
 
+    def model(
+        self,
+        embedding_dim: int = ta.Param(
+            default=8,
+            help="The size of the embeddings for the nucleotides (N, A, G, C, T).",
+            tune=True,
+            tune_min=4,
+            tune_max=32,
+            log=True,
+        ),
+        filters: int = ta.Param(
+            default=256,
+            help="The number of filters in each of the 1D convolution layers. These are concatenated together",
+        ),
+        cnn_layers: int = ta.Param(
+            default=6,
+            help="The number of 1D convolution layers.",
+            tune=True,
+            tune_min=2,
+            tune_max=6,
+        ),
+        kernel_size_maxpool: int = ta.Param(
+            default=2,
+            help="The size of the pooling before going to the LSTM.",
+        ),
+        lstm_dims: int = ta.Param(default=256, help="The size of the hidden layers in the LSTM in both directions."),
+        final_layer_dims: int = ta.Param(
+            default=0, help="The size of a dense layer after the LSTM. If this is zero then this layer isn't used."
+        ),
+        dropout: float = ta.Param(
+            default=0.5,
+            help="The amount of dropout to use. (not currently enabled)",
+            tune=True,
+            tune_min=0.0,
+            tune_max=1.0,
+        ),
+        final_bias: bool = ta.Param(
+            default=True,
+            help="Whether or not to use bias in the final layer.",
+            tune=True,
+        ),
+        cnn_only: bool = True,
+        kernel_size: int = ta.Param(
+            default=3, help="The size of the kernels for CNN only classifier.", tune=True, tune_choices=[3, 5, 7, 9]
+        ),
+        cnn_dims_start: int = ta.Param(
+            default=64,
+            help="The size of the number of filters in the first CNN layer.",
+            tune=True,
+            log=True,
+            tune_min=32,
+            tune_max=1024,
+        ),
+        factor: float = ta.Param(
+            default=2.0,
+            help="The factor to multiply the number of filters in the CNN layers each time it is downscaled.",
+            tune=True,
+            log=True,
+            tune_min=0.5,
+            tune_max=4.0,
+        ),
+    ) -> nn.Module:
+        """
+        Creates a deep learning model for the Corgi to use.
+        Returns:
+            nn.Module: The created model.
+        """
+        # TODO - get pretrained
+        return ConvClassifier(
+            num_embeddings=5,  # i.e. the size of the vocab which is N, A, C, G, T
+            kernel_size=kernel_size,
+            factor=factor,
+            cnn_layers=cnn_layers,
+            num_classes=self.classification_tree.layer_size,
+            kernel_size_maxpool=kernel_size_maxpool,
+            final_bias=final_bias,
+            dropout=dropout,
+            cnn_dims_start=cnn_dims_start,
+        )
+
     def monitor(self):
-        return "greedy_accuracy_depth_two"
+        return "greedy_f1_score"
 
     def output_results(
         self, 
