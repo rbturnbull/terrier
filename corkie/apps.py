a73a06662e0769766d433f308f3ac60e6bea29c4
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2023-04-05 13:16:38 +1000

    :zap: adding repeat_masker_type_id accuracy

--- corkie/apps.py
+++ corkie/apps.py
@@ -6,8 +6,19 @@ import torchapp as fa
 from rich.console import Console
 console = Console()
 from hierarchicalsoftmax import HierarchicalSoftmaxLoss, SoftmaxNode
-from hierarchicalsoftmax import metrics
+from hierarchicalsoftmax import metrics, inference
 import csv
+import torch
+
+
+def greedy_attribute_accuracy(prediction_tensor, target_tensor, root, attribute):
+    prediction_nodes = inference.greedy_predictions(prediction_tensor=prediction_tensor, root=root)
+    prediction_attributes = torch.as_tensor( [getattr(node, attribute) for node in prediction_nodes], dtype=int).to(target_tensor.device)
+
+    target_nodes = [root.node_list[target] for target in target_tensor]
+    target_attributes = torch.as_tensor( [getattr(node, attribute) for node in target_nodes], dtype=int).to(target_tensor.device)
+
+    return (prediction_attributes == target_attributes).float().mean()
 
 
 class Corkie(fa.TorchApp):
@@ -18,6 +29,7 @@ class Corkie(fa.TorchApp):
         super().__init__(*args, **kwargs)
         self.classification_tree = SoftmaxNode(name="root")
         self.classification_nodes = {}
+        self.repeatmasker_types = []
         with open(Path(__file__).parent/"data/TEClasses.tsv") as f:
             reader = csv.DictReader(f, delimiter='\t')
             for row in reader:
@@ -29,8 +41,23 @@ class Corkie(fa.TorchApp):
                     parent_lineage_string = ";".join(lineage[:-1])
                     parent = self.classification_nodes[parent_lineage_string]
                 
-                node = SoftmaxNode(name=lineage[-1], parent=parent, title=row['title'])
+                repeat_masker_type = row['repeatmasker_type']
+                repeat_masker_type_id = None
+                if repeat_masker_type:
+                    if not repeat_masker_type in self.repeatmasker_types:
+                        self.repeatmasker_types.append(repeat_masker_type)
+                    repeat_masker_type_id = self.repeatmasker_types.index(repeat_masker_type)
+
+                node = SoftmaxNode(
+                    name=lineage[-1], 
+                    parent=parent, 
+                    title=row['title'], 
+                    repeat_masker_type=repeat_masker_type,
+                    repeat_masker_type_id=repeat_masker_type_id,
+                )
                 self.classification_nodes[lineage_string] = node
+
+        self.repeatmasker_type_dict = {key: value for key, value in enumerate(self.repeatmasker_types)}
 
         try:
             text_file = Path("classification_tree.txt")
@@ -66,6 +93,7 @@ class Corkie(fa.TorchApp):
             partial(metrics.greedy_f1_score, root=self.classification_tree),
             partial(metrics.greedy_accuracy_depth_one, root=self.classification_tree), 
             partial(metrics.greedy_accuracy_depth_two, root=self.classification_tree), 
+            partial(greedy_attribute_accuracy, root=self.classification_tree, attribute="repeat_masker_type_id"), 
         ]
 
     def monitor(self):
