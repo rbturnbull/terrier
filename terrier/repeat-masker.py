e3fe4686bd3946f1986c3cd13751646cee0da8e0
Robert Turnbull <rturnbull@rdl-ejx5s7.desktop.cloud.unimelb.edu.au>
2023-11-15 13:08:58 +1100

    :zap: updating way to create repeatmasker seqtree file

--- terrier/repeat-masker.py
+++ terrier/repeat-masker.py
@@ -4,33 +4,20 @@ from hierarchicalsoftmax import SoftmaxNode
 from Bio import SeqIO
 from typing import Dict
 import csv
-
+import toml
 from corgi.seqtree import SeqTree #, AlreadyExists
+from collections import Counter
 
 app = typer.Typer()
 
-
-def create_mapping(file:Path) -> Dict[str, str]:
-    repbase_to_repeatmasker = dict()
-    with open(file) as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            repeatmasker_str = row['repeatmasker_type']
-            if row['repeatmasker_subtype']:
-                repeatmasker_str += "/" + row['repeatmasker_subtype']
-            if not repeatmasker_str:
-                repeatmasker_str = "Unknown"
-
-            repbase_to_repeatmasker[row['repbase'].strip()] = repeatmasker_str
-
-    return repbase_to_repeatmasker
-
-
 @app.command()
 def create_repbase_seqtree_repeatmasker(output:Path, repbase:Path, label_smoothing:float=0.0, gamma:float=0.0, partitions:int=5):
+    with open(Path(__file__).parent/"data/repbase-to-repeatmasker.toml", "r") as f:
+        mapping = toml.load(f)
 
-    mapping = create_mapping(Path(__file__).parent/"data/repbase-to-repeatmasker2.csv")
-
+    mapped_counter = Counter()
+    not_mapped_counter = Counter()
+    
     # Create Tree From Text File
     classification_tree = SoftmaxNode(name="root", label_smoothing=label_smoothing, gamma=gamma)
     classification_nodes = {}
@@ -57,7 +44,10 @@ def create_repbase_seqtree_repeatmasker(output:Path, repbase:Path, label_smoothi
                     classification = components[1]
 
                 if classification not in mapping:
+                    not_mapped_counter.update([classification])
                     continue
+
+                mapped_counter.update([classification])
 
                 repeat_name = mapping[classification]
                 if repeat_name == "Unknown":
@@ -83,7 +73,14 @@ def create_repbase_seqtree_repeatmasker(output:Path, repbase:Path, label_smoothi
 
                 count += 1
 
+    print("repbase,count,mapped,repeat_masker")
+    for classification,count in mapped_counter.most_common():
+        print(classification,count,1, mapping[classification], sep=",")
+    for classification,count in not_mapped_counter.most_common():
+        print(classification,count,0, "", sep=",")
+
     seqtree.save(output)
+    seqtree.classification_tree.render(print=1)
 
 
 if __name__ == "__main__":
